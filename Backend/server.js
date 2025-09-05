850c631cdbf53adb7bfafdd2300aeeca2ebfcba6
Pankaj Baduwal <baduwalpankaj@gmail.com>
2025-09-05 12:35:47 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = https://aapagpt-backend.onrender.com;
 
 app.use(express.json());
 app.use(cors());
